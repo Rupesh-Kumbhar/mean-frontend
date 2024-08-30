5035889e5623ab10d24d7d568d2931b265bf247c
RUPESH KUMBHAR <rupesh.p.kumbhar@gmail.com>
2024-08-30 13:04:26 +0530

    added employee cards and used ngFor

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+// import { NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-employee',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.scss'
 })
+
 export class EmployeeComponent {
+  employees = [
+    {name : "n1" , position : "p1", dept : "d1" },
+    {name : "n2" , position : "p2", dept : "d2" },
+    {name : "n3" , position : "p3", dept : "d3" },
+    {name : "n4" , position : "p4", dept : "d4" },
+  ]
 
 }
